0ed756d0539504f4ea41aa3e7edef5eafb080a44
dnhieu992 <daonguyenhieu090492@gmail.com>
2026-03-29 21:29:59 +0700

    feat: inject X-Tenant-Slug header into all axios requests

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,11 +1,24 @@
-import axios from "axios";
+// src/lib/axios.ts
+import axios from 'axios'
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
+})
+
+function getTenantSlugFromCookie(): string | undefined {
+  if (typeof document === 'undefined') return undefined // SSR guard
+  return document.cookie
+    .split('; ')
+    .find((row) => row.startsWith('tenant-slug='))
+    ?.split('=')[1]
+}
 
 api.interceptors.request.use((config) => {
-  return config;
-});
+  const slug = getTenantSlugFromCookie()
+  if (slug) {
+    config.headers['X-Tenant-Slug'] = slug
+  }
+  return config
+})
 
-export default api;
+export default api
